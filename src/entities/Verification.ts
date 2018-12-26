6dc8aef0f3219a53f980d556f085fa79c5f93053
root <root@localhost.localdomain>
2018-12-26 08:34:25 -0500

    add method generating key

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,12 +1,15 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert } from "typeorm";
 import {verificationTarget} from "../types/types";
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
 
 
 @Entity()
 class Verification extends BaseEntity{
     @PrimaryGeneratedColumn()
     id:number;
-    @Column({type:"text"})
+    @Column({type:"text", enum:[PHONE,EMAIL]})
     target: verificationTarget
     @Column({type:"text"})
     payload:string;
@@ -19,6 +22,15 @@ class Verification extends BaseEntity{
     createdAt:string;
     @UpdateDateColumn()
     updatedAt:string;
+
+    @BeforeInsert()
+    createKey(): void{
+        if(this.target===PHONE){
+            this.key = Math.floor(Math.random()*100000).toString();
+        } else if (this.target===EMAIL){
+            this.key = Math.random().toString(36).substr(2);
+        }
+    }
     
 }
 
