98de4caa0a238d560d2544271d0d9266a4b078c8
root <root@localhost.localdomain>
2019-01-02 01:41:03 -0500

    add resolver : RequestEmailVerification

---
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -0,0 +1,48 @@
+import {Resolvers} from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import Verification from "../../../entities/Verification";
+import User from "../../../entities/User";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+import { RequestEmailVerificationResponse } from "src/types/graph";
+
+const resolvers: Resolvers = {
+    Mutation:{
+        RequestEmailVerification: privateResolver(
+            async(_,__,{req}):Promise<RequestEmailVerificationResponse> =>{
+                const user: User = req.user;
+                if(user.email){
+                try{
+                    const oldVerification = await Verification.findOne({payload:user.email});
+                    if(oldVerification){
+                        oldVerification.remove();
+                    }
+                    const newVerification = await Verification.create({
+                        payload:user.email,
+                        target:"EMAIL"
+                    }).save();
+                    await sendVerificationEmail(user.email, user.fullName, newVerification.key);
+                    return{
+                        ok:true,
+                        error:null
+                    }
+
+                } catch(error) {
+                    return{
+                        ok:false,
+                        error:error.message
+                    }
+
+                }
+                
+                } else {
+                    return {
+                        ok:false,
+                        error: "Your user has no email"
+                    };
+                }
+            }
+        )
+    }
+};
+
+export default resolvers;
