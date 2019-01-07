f31721ea3d80e2a2e2cfa6b5275890db25412d7d
root <root@localhost.localdomain>
2019-01-07 06:24:12 -0500

    change GetNearbyRides -> GetNearbyRide

--- src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
+++ src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
@@ -2,32 +2,40 @@ import {Resolvers} from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
 import Ride from "../../../entities/Ride";
 import User from "../../../entities/User";
-import { GetNearbyRidesResponse } from "src/types/graph";
+import { GetNearbyRideResponse } from "src/types/graph";
 import { getRepository, Between } from "typeorm";
 
 const resolvers:Resolvers = {
     Query:{
-        GetNearbyRides:privateResolver(
-            async(_,__,{req}):Promise<GetNearbyRidesResponse> =>{
+        GetNearbyRide:privateResolver(
+            async(_,__,{req}):Promise<GetNearbyRideResponse> =>{
                 try{
                     const user:User = req.user;
                     if(user.isDriving){
                         const {lastLat, lastLng} = user;
-                        const rides = await getRepository(Ride).find({
+                        const ride = await getRepository(Ride).findOne({
                             status:"REQUESTING",
                             pickUpLat:Between(lastLat-0.05, lastLat+0.05),
                             pickUpLng:Between(lastLng-0.05, lastLng+0.05)
                         })
-                        return{
-                            ok:true,
-                            error:null,
-                            rides
+                        if(ride){
+                            return{
+                                ok:true,
+                                error:null,
+                                ride
+                            }
+                        } else {
+                            return{
+                                ok:true,
+                                error:null,
+                                ride:null
+                            }
                         }
                     } else {
                         return{
                             ok:false,
                             error:"You are not a driver!",
-                            rides:null
+                            ride:null
                         }
                     }
 
@@ -35,7 +43,7 @@ const resolvers:Resolvers = {
                     return{
                         ok:false,
                         error:error.message,
-                        rides:null
+                        ride:null
                     }
                 }
 
