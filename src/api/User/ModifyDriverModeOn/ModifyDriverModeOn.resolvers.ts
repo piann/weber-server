99332cbaf7dc84d5c4808008921b23ce82654a26
root <root@localhost.localdomain>
2019-01-25 23:57:11 -0500

    fix mutation to ModifyDriverMode so that it contains user.isDriving

--- src/api/User/ModifyDriverModeOn/ModifyDriverModeOn.resolvers.ts
+++ src/api/User/ModifyDriverModeOn/ModifyDriverModeOn.resolvers.ts
@@ -1,17 +1,22 @@
 import {Resolvers} from "../../../types/resolvers";
 import User from "../../../entities/User";
 import privateResolver from "../../../utils/privateResolver";
-import { ModifyDriverModeOnResponse, ModifyDriverModeOnMutationArgs } from "src/types/graph";
+import { ModifyDrivingStatusMutationArgs, ModifyDrivingStatusResponse } from "src/types/graph";
 
 const resolvers: Resolvers = {
     Mutation:{
-        ModifyDriverModeOn: privateResolver(
-            async(_,args:ModifyDriverModeOnMutationArgs,{req}):Promise<ModifyDriverModeOnResponse>=>{
+        ModifyDrivingStatus: privateResolver(
+            async(_,args:ModifyDrivingStatusMutationArgs,{req}):Promise<ModifyDrivingStatusResponse>=>{
                 const user:User = req.user;
                 if(user){
                     try{
+                        const driverModeOn = args.driverModeOn
+                        let isDriving = user.isDriving;
+                        if(args.isDriving !== null){
+                            isDriving = args.isDriving;
+                        }
                         delete user.updatedAt // updatedAt is automatically added, so is should be excluded preventing multiple assignments to same column
-                        await User.update({id:user.id},{...user, driverModeOn:args.driverModeOn});
+                        await User.update({id:user.id},{...user, driverModeOn, isDriving});
                         return{
                             ok:true,
                             error:null
