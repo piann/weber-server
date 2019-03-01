4f8f56730df7298463cb02664adcfcb12e86abba
root <root@localhost.localdomain>
2019-03-01 10:00:48 -0500

    heroku debug trial#9

--- dist/index.js
+++ dist/index.js
@@ -34,20 +34,14 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
-var dotenv = __importStar(require("dotenv"));
-dotenv.config();
+require('dotenv').config();
+///require('now-env');
+console.log(process.env);
 var typeorm_1 = require("typeorm");
 var ormConfig_1 = __importDefault(require("./ormConfig"));
 var app_1 = __importDefault(require("./app"));
